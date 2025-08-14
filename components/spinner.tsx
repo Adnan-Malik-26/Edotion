a1066ae42eda64943f18d42bb0e009e84a323b80
Adnan-Malik-26 <eddymalikadnan@gmail.com>
2025-08-14 23:03:25 +0530

    Add

--- components/spinner.tsx
+++ components/spinner.tsx
@@ -1,7 +1,5 @@
 import { Loader } from "lucide-react";
-
 import { cva, type VariantProps } from "class-variance-authority";
-
 import { cn } from "@/lib/utils";
 
 const spinnerVariants = cva(
@@ -12,22 +10,18 @@ const spinnerVariants = cva(
         default: "h-4 w-4",
         sm: "h-2 w-2",
         lg: "h-6 w-6",
-        icon: "h-10 w-10"
-      }
+        icon: "h-10 w-10",
+      },
     },
     defaultVariants: {
       size: "default",
-
     },
-  },
+  }
 );
 
-interface spinnerProps extends VariantProps<typeof spinnerVariants> { };
+// ✅ Use a type alias instead of an empty interface
+type SpinnerProps = VariantProps<typeof spinnerVariants>;
 
-export const Spinner = ({
-  size,
-}: spinnerProps) => {
-  return (
-    <Loader className={cn(spinnerVariants({ size }))} />
-  );
+export const Spinner = ({ size }: SpinnerProps) => {
+  return <Loader className={cn(spinnerVariants({ size }))} />;
 };
